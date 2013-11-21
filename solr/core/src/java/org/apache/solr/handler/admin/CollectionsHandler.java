c1c2b9630519df0662b7763c59bb5826dbe23dd5
Erick Erickson <erick@apache.org>
2013-11-21 00:58:24 +0000

    SOLR-5208: Support for the setting of core.properties key/values at create-time on Collections API
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1543996 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit e81572a37d7bb2b356adc1f15718cb61227ec2b7)
    
    Conflicts:
    	solr/CHANGES.txt

--- solr/core/src/java/org/apache/solr/handler/admin/CollectionsHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CollectionsHandler.java
@@ -314,6 +314,7 @@ public class CollectionsHandler extends RequestHandlerBase {
         SHARDS_PROP,
         "router.");
 
+    copyPropertiesIfNotNull(req.getParams(), props);
 
     ZkNodeProps m = new ZkNodeProps(props);
     handleResponse(OverseerCollectionProcessor.CREATECOLLECTION, m, rsp);
@@ -338,6 +339,7 @@ public class CollectionsHandler extends RequestHandlerBase {
 
     Map<String, Object> map = makeMap(QUEUE_OPERATION, CREATESHARD);
     copyIfNotNull(req.getParams(),map,COLLECTION_PROP, SHARD_ID_PROP, REPLICATION_FACTOR,CREATE_NODE_SET);
+    copyPropertiesIfNotNull(req.getParams(), map);
     ZkNodeProps m = new ZkNodeProps(map);
     handleResponse(CREATESHARD, m, rsp);
   }
@@ -368,7 +370,18 @@ public class CollectionsHandler extends RequestHandlerBase {
     }
 
   }
-  
+
+  private void copyPropertiesIfNotNull(SolrParams params, Map<String, Object> props) {
+    Iterator<String> iter =  params.getParameterNamesIterator();
+    while (iter.hasNext()) {
+      String param = iter.next();
+      if (param.indexOf(OverseerCollectionProcessor.COLL_PROP_PREFIX) != -1) {
+        props.put(param, params.get(param));
+      }
+    }
+  }
+
+
   private void handleDeleteShardAction(SolrQueryRequest req,
       SolrQueryResponse rsp) throws InterruptedException, KeeperException {
     log.info("Deleting Shard : " + req.getParamString());
@@ -416,6 +429,7 @@ public class CollectionsHandler extends RequestHandlerBase {
     if (rangesStr != null)  {
       props.put(CoreAdminParams.RANGES, rangesStr);
     }
+    copyPropertiesIfNotNull(req.getParams(), props);
 
     ZkNodeProps m = new ZkNodeProps(props);
 
