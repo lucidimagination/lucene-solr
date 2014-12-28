21c46d8e2f1a8742a9dabfa69eea34d23e5f45d6
Varun Thacker <varunthacker1989@gmail.com>
2014-12-28 14:53:26 +0530

    SOLR-6879 - Backport

--- solr/core/src/java/org/apache/solr/cloud/OverseerCollectionProcessor.java
+++ solr/core/src/java/org/apache/solr/cloud/OverseerCollectionProcessor.java
@@ -147,7 +147,8 @@ public class OverseerCollectionProcessor implements Runnable, Closeable {
 
   public static final String COLL_PROP_PREFIX = "property.";
 
-  public static final Set<String> KNOWN_CLUSTER_PROPS = ImmutableSet.of(ZkStateReader.LEGACY_CLOUD, ZkStateReader.URL_SCHEME);
+  public static final Set<String> KNOWN_CLUSTER_PROPS = ImmutableSet.of(ZkStateReader.LEGACY_CLOUD, ZkStateReader.URL_SCHEME,
+      ZkStateReader.AUTO_ADD_REPLICAS);
 
   public static final Map<String,Object> COLL_PROPS = ZkNodeProps.makeMap(
       ROUTER, DocRouter.DEFAULT_NAME,
